f54ed79cbb3c984c55b1ed34df7bf0d3604fd920
dev-fatoh <fatohnasr360@gmail.com>
2025-09-13 05:25:36 +0300

    Disable player name input and button after setting the name to prevent multiple submissions. Add responsive styles for player section and challenge components for better mobile experience.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,20 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function Player({ name, setName }) {
+  const [visible, setVisible] = useState(false);
   const playerName = useRef();
   function handleClick() {
     setName(playerName.current.value);
     playerName.current.value = "";
+    setVisible(true);
   }
   return (
     <section id="player">
       <h2>Welcome {name ?? "unknown entity"}</h2>
       <p>
-        <input autoFocus ref={playerName} type="text" />
-        <button onClick={handleClick}>Set Name</button>
+        <input disabled={visible} autoFocus ref={playerName} type="text" />
+        <button disabled={visible} onClick={handleClick}>
+          Set Name
+        </button>
       </p>
     </section>
   );
